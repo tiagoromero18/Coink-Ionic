bab1f1634a1c3c9d866dde315130221ef03ea4e7
Santiago Romero Lozano <tiago.romero18@gmail.com>
2024-10-10 02:48:12 -0500

    FirstGlance

--- Coink/src/app/home/home.page.ts
+++ Coink/src/app/home/home.page.ts
@@ -9,4 +9,22 @@ export class HomePage {
 
   constructor() {}
 
+  ngOnInit() {
+
+    const contentElement = document.getElementById('content');
+    if(contentElement){
+      contentElement.style.setProperty('--background-color', '#1EEA00');
+    }
+
+    setTimeout(() => {
+      const introElement = document.getElementById('intro');
+      const mainContent = document.getElementById('main-content');
+
+      if (introElement && mainContent && contentElement) {
+        introElement.classList.add('hidden');
+        mainContent.classList.remove('hidden');
+        contentElement.style.setProperty('--background-color', 'white');
+      }
+    }, 2000); // 0.5 seconds
+  }
 }
